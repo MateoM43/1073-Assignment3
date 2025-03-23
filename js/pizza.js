04ff158fcb7ca0acb2c91f66731dbb72201ac61d
MateoM43 <mateomatti06@gmail.com>
2025-03-23 15:41:55 -0400

    Update pizza.js

--- js/pizza.js
+++ js/pizza.js
@@ -1,11 +1,13 @@
+// This defines a pizza class to represent a pizza order
 class Pizza {
+    // Constructor to initialize pizza properties
     constructor(crust, sauce, toppings, size) {
         this.crust = crust;
         this.sauce = sauce;
         this.toppings = toppings;
         this.size = size;
     }
-
+    // Method to describe the pizza order as a string
     describe() {
         return `You ordered a ${this.size} pizza with ${this.crust}, ${this.sauce} sauce, and toppings: ${this.toppings.join(', ')}.`;
     }
